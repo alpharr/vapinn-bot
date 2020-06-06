0581e38174a1351b05b88b85d8d921e3dfa047d9
alpharr <tcmpvh8@gmail.com>
2020-06-06 12:02:08 -0400

    aa

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const bot = new Discord.Client();
 
 
 
-const PREFIX = "/";
+const PREFIX = "v!";
 
 bot.on("ready", () =>{
-    console.log(">> Linnex, Loaded. <<");
-    bot.user.setActivity("linnex.club || /help");
+    console.log(">> Vapinn bot, Loaded. <<");
+    bot.user.setActivity("Vapinn || v!help");
 })
 
 bot.on("message", message=>{
@@ -16,15 +16,14 @@ bot.on("message", message=>{
 
     switch(args[0]){
         case "ip":
-            message.channel.send("> **IP:** `Linnex.club`");
-            console.log("Hey! Someone executed the /ip command!");
+            message.channel.send("> **IP:** `UNAVAILABLE!`");
         break;
         case "help":
-            message.channel.send("> ‣ **Help** \n > _ _ \n > **/ip** \n > **/help** \n > **/apply** \n > _ _");
+            message.channel.send("> ‣ **Help** \n > _ _ \n > **v!ip** \n > **v!help** \n > **v!apply** \n > _ _");
         break;
         case "apply":
-            message.channel.send("We have sent you a message in DMs. If you have DMs off, you will need to enable them. Thank you!");
-            message.author.send("Staff applications guidelines URL: https://docs.google.com/document/d/16n0oGAoVG5p_By4Sfmyjf97HgW6rRwuzIsMY4BpVw-E/edit?usp=sharing \n - YouTuber Apps URL: https://docs.google.com/forms/d/e/1FAIpQLSfwCxUJIPMeKJKtDCO8bnOgIWy_wB_QbHecuYE7jbXQ16pAnQ/viewform \n - Staff apps URL: https://docs.google.com/forms/d/e/1FAIpQLSfv0aTscDr7WCIO8BpZhNqsS7XnBjtU9LgRE9S5yG52IORl_A/viewform?usp=sf_link \n - Ban appeals: https://forms.gle/kShUxptQ4UZTLd4m7 \n - Builder Apps URL: https://docs.google.com/forms/d/e/1FAIpQLSeAD7Fo09n-fw4gCJneMMfrzZDlqzK83cZbCb256JxsGyMI8g/viewform");
+            message.channel.send("Staff Applications are not open yet!");
+        //    message.author.send("")
         break;
 
     }
@@ -38,13 +37,13 @@ bot.on('message', function(message){
 
 bot.on('message', function(message){
     if(message.content == "hello"){
-        message.channel.send("Hey there!");
+        message.channel.send("Hey");
     }
 })
 
 bot.on('message', function(message){
     if(message.content == "whats the ip"){
-        message.channel.send("> **IP:** `Linnex.club`");
+        message.channel.send("> **IP:** `UNAVAILABLE!`");
     }
 })
 
